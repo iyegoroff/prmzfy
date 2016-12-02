64f2bb5fb42a8e0ab09b6d0505252ff1656326b0
Igor Yegorov <iegoroff@gmail.com>
2016-12-03 01:54:08 +0300

    sauce labs - 6

--- tests/intern-ci.js
+++ tests/intern-ci.js
@@ -1,9 +1,9 @@
 define({
     environments: [
-        { browserName: 'chrome', version: ['33', 'latest'], platform: ['Linux', 'Windows 10', 'MacOS El Capitan 10.11'] },
-        { browserName: 'firefox', version: ['29', 'latest'], platform: ['Linux', 'Windows 10', 'MacOS El Capitan 10.11'] },
-        { browserName: 'msedge', version: ['12', 'latest'], platform: 'Windows 10' },
-        { browserName: 'safari', version: ['7.1', 'latest'], platform: 'MacOS El Capitan 10.11' }
+        { browserName: 'chrome', version: ['33', 'latest'], platform: ['Linux', 'Windows 10', 'OS X 10.11'] },
+        { browserName: 'firefox', version: ['29', 'latest'], platform: ['Linux', 'Windows 10', 'OS X 10.11'] },
+        { browserName: 'MicrosoftEdge', version: ['13', 'latest'], platform: 'Windows 10' },
+        { browserName: 'safari', version: ['9', 'latest'], platform: 'OS X 10.11' }
     ],
 
 	tunnel: 'SauceLabsTunnel',
