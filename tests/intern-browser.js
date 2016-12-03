cc5113ed64997140ae152f54e567d4c6928f8a95
Igor Yegorov <iegoroff@gmail.com>
2016-12-03 19:41:19 +0300

    add

---
+++ tests/intern-browser.js
@@ -0,0 +1,52 @@
+define({
+    environments: [
+        // { 
+        //     browserName: 'chrome', 
+        //     version: ['33', '48'], 
+        //     platform: ['Linux', 'Windows 10', 'OS X 10.11'],
+        //     recordVideo: false,
+        //     recordScreenshots: false
+        // },
+        // { 
+        //     browserName: 'firefox', 
+        //     version: ['29', '45'], 
+        //     platform: ['Linux', 'Windows 10', 'OS X 10.11'],
+        //     recordVideo: false,
+        //     recordScreenshots: false
+        // },
+        // { 
+        //     browserName: 'MicrosoftEdge', 
+        //     version: ['13', '14'], 
+        //     platform: 'Windows 10',
+        //     recordVideo: false,
+        //     recordScreenshots: false 
+        // },
+        // { 
+        //     browserName: 'safari', 
+        //     version: ['9', '10'], 
+        //     platform: 'OS X 10.11',
+        //     recordVideo: false,
+        //     recordScreenshots: false 
+        // },
+        {
+            browserName: 'iOS',
+            recordVideo: false,
+            recordScreenshots: false
+        },
+        // { 
+        //     browserName: 'android',
+        //     recordVideo: false,
+        //     recordScreenshots: false
+        // }
+    ],
+
+	tunnel: 'SauceLabsTunnel',
+
+    maxConcurrency: 5,
+
+	reporters: ['Runner', 'Console'],
+
+	suites: ['tests/unit/index'],
+
+	excludeInstrumentation: /^(?:tests|node_modules)\//
+});
