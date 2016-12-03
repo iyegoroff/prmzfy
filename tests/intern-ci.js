ae72bc7c9e08b1eb217aeebcc59af2ebc66f76c2
Igor Yegorov <iegoroff@gmail.com>
2016-12-03 19:02:50 +0300

    sauce labs android

--- tests/intern-ci.js
+++ tests/intern-ci.js
@@ -1,33 +1,33 @@
 define({
     environments: [
-        { 
-            browserName: 'chrome', 
-            version: ['33', '48'], 
-            platform: ['Linux', 'Windows 10', 'OS X 10.11'],
-            recordVideo: false,
-            recordScreenshots: false
-        },
-        { 
-            browserName: 'firefox', 
-            version: ['29', '45'], 
-            platform: ['Linux', 'Windows 10', 'OS X 10.11'],
-            recordVideo: false,
-            recordScreenshots: false
-        },
-        { 
-            browserName: 'MicrosoftEdge', 
-            version: ['13', '14'], 
-            platform: 'Windows 10',
-            recordVideo: false,
-            recordScreenshots: false 
-        },
-        { 
-            browserName: 'safari', 
-            version: ['9', '10'], 
-            platform: 'OS X 10.11',
-            recordVideo: false,
-            recordScreenshots: false 
-        },
+        // { 
+        //     browserName: 'chrome', 
+        //     version: ['33', '48'], 
+        //     platform: ['Linux', 'Windows 10', 'OS X 10.11'],
+        //     recordVideo: false,
+        //     recordScreenshots: false
+        // },
+        // { 
+        //     browserName: 'firefox', 
+        //     version: ['29', '45'], 
+        //     platform: ['Linux', 'Windows 10', 'OS X 10.11'],
+        //     recordVideo: false,
+        //     recordScreenshots: false
+        // },
+        // { 
+        //     browserName: 'MicrosoftEdge', 
+        //     version: ['13', '14'], 
+        //     platform: 'Windows 10',
+        //     recordVideo: false,
+        //     recordScreenshots: false 
+        // },
+        // { 
+        //     browserName: 'safari', 
+        //     version: ['9', '10'], 
+        //     platform: 'OS X 10.11',
+        //     recordVideo: false,
+        //     recordScreenshots: false 
+        // },
         // {
         //     browserName: 'Safari',
         //     appiumVersion: '1.6.1',
@@ -38,16 +38,15 @@ define({
         //     platformVersion: '10.0',
         //     platformName: 'iOS'
         // },
-        // {
-        //     browserName: 'Browser',
-        //     appiumVersion: '1.5.3',
-        //     recordVideo: false,
-        //     recordScreenshots: false,
-        //     deviceName: 'Android Emulator',
-        //     deviceOrientation: 'portrait',
-        //     platformVersion: '5.1',
-        //     platformName: 'Android'
-        // }
+        {
+            browserName: 'Browser',
+            appiumVersion: '1.5.3',
+            recordVideo: false,
+            recordScreenshots: false,
+            deviceName: 'Android Emulator',
+            platformVersion: '5.1',
+            platformName: 'Android'
+        }
     ],
 
 	tunnel: 'SauceLabsTunnel',
