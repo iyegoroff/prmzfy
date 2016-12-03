d8713886bb5d41ab834cfa3875ca6c1ae51a60ae
Igor Yegorov <iegoroff@gmail.com>
2016-12-03 20:18:01 +0300

    sauce labs all

--- tests/intern-browser.js
+++ tests/intern-browser.js
@@ -1,33 +1,33 @@
 define({
     environments: [
-        // { 
-        //     browserName: 'chrome', 
-        //     version: ['33', '48'], 
-        //     platform: ['Linux', 'Windows 10', 'OS X 10.11'],
-        //     recordVideo: false,
-        //     recordScreenshots: false
-        // },
-        // { 
-        //     browserName: 'firefox', 
-        //     version: ['29', '45'], 
-        //     platform: ['Linux', 'Windows 10', 'OS X 10.11'],
-        //     recordVideo: false,
-        //     recordScreenshots: false
-        // },
-        // { 
-        //     browserName: 'MicrosoftEdge', 
-        //     version: ['13', '14'], 
-        //     platform: 'Windows 10',
-        //     recordVideo: false,
-        //     recordScreenshots: false 
-        // },
-        // { 
-        //     browserName: 'safari', 
-        //     version: ['9', '10'], 
-        //     platform: 'OS X 10.11',
-        //     recordVideo: false,
-        //     recordScreenshots: false 
-        // },
+        { 
+            browserName: 'chrome', 
+            version: ['33', '48'], 
+            platform: ['Linux', 'Windows 10', 'OS X 10.11'],
+            recordVideo: false,
+            recordScreenshots: false
+        },
+        { 
+            browserName: 'firefox', 
+            version: ['29', '45'], 
+            platform: ['Linux', 'Windows 10', 'OS X 10.11'],
+            recordVideo: false,
+            recordScreenshots: false
+        },
+        { 
+            browserName: 'MicrosoftEdge', 
+            version: ['13', '14'], 
+            platform: 'Windows 10',
+            recordVideo: false,
+            recordScreenshots: false 
+        },
+        { 
+            browserName: 'safari', 
+            version: ['9', '10'], 
+            platform: 'OS X 10.11',
+            recordVideo: false,
+            recordScreenshots: false 
+        },
         {
             browserName: 'iphone',
             recordVideo: false,
