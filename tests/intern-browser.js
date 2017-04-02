b21b1dfa7a4c0af02420a65575b304b1f5c6ca2c
Igor Yegorov <iegoroff@gmail.com>
2017-04-02 21:35:44 +0300

    restored sauce config [skip ci]

--- tests/intern-browser.js
+++ tests/intern-browser.js
@@ -19,61 +19,49 @@ define({
             deviceOrientation: 'portrait',
             platformVersion: ['4.4', '5.1', '6.0'],
             platformName: 'Android'
+        },
+        {
+            browserName: 'chrome',
+            version: ['31', '32', '57'],
+            platform: ['Windows 10'],
+            recordVideo: false,
+            recordScreenshots: false
+        },
+        {
+            browserName: 'firefox',
+            version: ['28', '29', '52'],
+            platform: ['Windows 10'],
+            recordVideo: false,
+            recordScreenshots: false
+        },
+        {
+            browserName: 'MicrosoftEdge',
+            version: ['13', '14'],
+            platform: 'Windows 10',
+            recordVideo: false,
+            recordScreenshots: false
+        },
+        {
+            browserName: 'safari',
+            version: ['9', '10'],
+            platform: 'OS X 10.11',
+            recordVideo: false,
+            recordScreenshots: false
+        },
+        {
+            browserName: 'safari',
+            version: ['7'],
+            platform: 'OS X 10.9',
+            recordVideo: false,
+            recordScreenshots: false
+        },
+        {
+            browserName: 'internet explorer',
+            version: ['11'],
+            platform: 'Windows 7',
+            recordVideo: false,
+            recordScreenshots: false
         }
-        // {
-        //     browserName: 'iphone',
-        //     version: ['8.1', '9.2'],
-        //     recordVideo: false,
-        //     recordScreenshots: false
-        // },
-        // {
-        //     browserName: 'android',
-        //     version: ['4.4', '5.1'],
-        //     recordVideo: false,
-        //     recordScreenshots: false
-        // },
-        // {
-        //     browserName: 'chrome',
-        //     version: ['31', '32', '54'],
-        //     platform: ['Windows 10'],
-        //     recordVideo: false,
-        //     recordScreenshots: false
-        // },
-        // {
-        //     browserName: 'firefox',
-        //     version: ['28', '29', '50'],
-        //     platform: ['Windows 10'],
-        //     recordVideo: false,
-        //     recordScreenshots: false
-        // },
-        // {
-        //     browserName: 'MicrosoftEdge',
-        //     version: ['13', '14'],
-        //     platform: 'Windows 10',
-        //     recordVideo: false,
-        //     recordScreenshots: false
-        // },
-        // {
-        //     browserName: 'safari',
-        //     version: ['9', '10'],
-        //     platform: 'OS X 10.11',
-        //     recordVideo: false,
-        //     recordScreenshots: false
-        // },
-        // {
-        //     browserName: 'safari',
-        //     version: ['7'],
-        //     platform: 'OS X 10.9',
-        //     recordVideo: false,
-        //     recordScreenshots: false
-        // },
-        // {
-        //     browserName: 'internet explorer',
-        //     version: ['11'],
-        //     platform: 'Windows 7',
-        //     recordVideo: false,
-        //     recordScreenshots: false
-        // }
     ],
 
     tunnel: 'SauceLabsTunnel',
