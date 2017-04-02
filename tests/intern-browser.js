aab49853607a3ea143e9da72662f8826de4b4234
Igor Yegorov <iegoroff@gmail.com>
2017-04-02 21:11:19 +0300

    sauce config update

--- tests/intern-browser.js
+++ tests/intern-browser.js
@@ -1,59 +1,79 @@
 define({
     environments: [
         {
-            browserName: 'iphone',
-            version: ['8.1', '9.2'],
+            browserName: 'Safari',
+            appiumVersion: '1.6.3',
             recordVideo: false,
-            recordScreenshots: false
+            recordScreenshots: false,
+            deviceName: 'iPhone Simulator',
+            deviceOrientation: 'portrait',
+            platformVersion: ['9.3', '10.2'],
+            platformName: 'iOS'
         },
         {
-            browserName: 'android',
-            version: ['4.4', '5.1'],
+            browserName: 'Browser',
+            appiumVersion: '1.6.3',
             recordVideo: false,
-            recordScreenshots: false
-        },
-        {
-            browserName: 'chrome',
-            version: ['31', '32', '54'],
-            platform: ['Windows 10'],
-            recordVideo: false,
-            recordScreenshots: false
-        },
-        {
-            browserName: 'firefox',
-            version: ['28', '29', '50'],
-            platform: ['Windows 10'],
-            recordVideo: false,
-            recordScreenshots: false
-        },
-        {
-            browserName: 'MicrosoftEdge',
-            version: ['13', '14'],
-            platform: 'Windows 10',
-            recordVideo: false,
-            recordScreenshots: false
-        },
-        {
-            browserName: 'safari',
-            version: ['9', '10'],
-            platform: 'OS X 10.11',
-            recordVideo: false,
-            recordScreenshots: false
-        },
-        {
-            browserName: 'safari',
-            version: ['7'],
-            platform: 'OS X 10.9',
-            recordVideo: false,
-            recordScreenshots: false
-        },
-        {
-            browserName: 'internet explorer',
-            version: ['11'],
-            platform: 'Windows 7',
-            recordVideo: false,
-            recordScreenshots: false
+            recordScreenshots: false,
+            deviceName: 'Android Emulator',
+            deviceOrientation: 'portrait',
+            platformVersion: ['4.4', '5.1', '6.0'],
+            platformName: 'Android'
         }
+        // {
+        //     browserName: 'iphone',
+        //     version: ['8.1', '9.2'],
+        //     recordVideo: false,
+        //     recordScreenshots: false
+        // },
+        // {
+        //     browserName: 'android',
+        //     version: ['4.4', '5.1'],
+        //     recordVideo: false,
+        //     recordScreenshots: false
+        // },
+        // {
+        //     browserName: 'chrome',
+        //     version: ['31', '32', '54'],
+        //     platform: ['Windows 10'],
+        //     recordVideo: false,
+        //     recordScreenshots: false
+        // },
+        // {
+        //     browserName: 'firefox',
+        //     version: ['28', '29', '50'],
+        //     platform: ['Windows 10'],
+        //     recordVideo: false,
+        //     recordScreenshots: false
+        // },
+        // {
+        //     browserName: 'MicrosoftEdge',
+        //     version: ['13', '14'],
+        //     platform: 'Windows 10',
+        //     recordVideo: false,
+        //     recordScreenshots: false
+        // },
+        // {
+        //     browserName: 'safari',
+        //     version: ['9', '10'],
+        //     platform: 'OS X 10.11',
+        //     recordVideo: false,
+        //     recordScreenshots: false
+        // },
+        // {
+        //     browserName: 'safari',
+        //     version: ['7'],
+        //     platform: 'OS X 10.9',
+        //     recordVideo: false,
+        //     recordScreenshots: false
+        // },
+        // {
+        //     browserName: 'internet explorer',
+        //     version: ['11'],
+        //     platform: 'Windows 7',
+        //     recordVideo: false,
+        //     recordScreenshots: false
+        // }
     ],
 
     tunnel: 'SauceLabsTunnel',
